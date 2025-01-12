db26287c5aa5b842625a8b4b2eafbcc23f80a0e0
Piotr Podusowski <podusowski@gmail.com>
2025-01-12 17:04:32 +0100

    warning and log removed

--- demo_android/rust/src/lib.rs
+++ demo_android/rust/src/lib.rs
@@ -6,7 +6,6 @@ fn android_main(
     app: egui_winit::winit::platform::android::activity::AndroidApp,
 ) -> Result<(), Box<dyn std::error::Error>> {
     use eframe::{NativeOptions, Renderer};
-    use egui_winit::winit::platform::android::EventLoopBuilderExtAndroid;
 
     android_logger::init_once(
         android_logger::Config::default()
@@ -16,7 +15,11 @@ fn android_main(
     let mut options = NativeOptions::default();
     options.renderer = Renderer::Wgpu;
     options.android_app = Some(app);
-    eframe::run_native("Walkers", options, Box::new(|_| Ok(Box::new(MyApp::new()))))?;
+    eframe::run_native(
+        "egui keyboard demo",
+        options,
+        Box::new(|_| Ok(Box::new(MyApp::new()))),
+    )?;
 
     Ok(())
 }
@@ -35,7 +38,6 @@ impl MyApp {
 
 impl eframe::App for MyApp {
     fn update(&mut self, _ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        log::info!("Hello, Android!");
         CentralPanel::default().show(_ctx, |ui| {
             ui.heading("egui keyboard demo");
             ui.text_edit_singleline(&mut self.text);
