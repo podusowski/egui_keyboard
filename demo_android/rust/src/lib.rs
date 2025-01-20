dc9632ce6766c08aec5d96af48fa4f3d0964f2f7
Piotr Podusowski <podusowski@gmail.com>
2025-01-20 20:09:57 +0100

    demo lib

--- demo_android/rust/src/lib.rs
+++ demo_android/rust/src/lib.rs
@@ -1,5 +1,4 @@
 use eframe::egui::{self, CentralPanel};
-use egui_keyboard::Keyboard;
 
 #[cfg(target_os = "android")]
 #[no_mangle]
@@ -23,29 +22,4 @@ fn android_main(
     )?;
 
     Ok(())
-}
-
-struct MyApp {
-    text: String,
-    keyboard: Keyboard,
-}
-
-impl MyApp {
-    fn new() -> Self {
-        Self {
-            text: "edit me".to_string(),
-            keyboard: Keyboard::default(),
-        }
-    }
-}
-
-impl eframe::App for MyApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        CentralPanel::default().show(ctx, |ui| {
-            self.keyboard.pump_events(ctx);
-            ui.heading("egui keyboard demo");
-            ui.text_edit_singleline(&mut self.text);
-            self.keyboard.show(ctx);
-        });
-    }
 }
