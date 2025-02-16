a3e4ccee14c42185dfa9e27ffc5fc448cb858276
Piotr Podusowski <podusowski@gmail.com>
2025-02-16 14:02:08 +0100

    simplify demo

--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -3,7 +3,6 @@ use egui_keyboard::Keyboard;
 
 pub struct MyApp {
     text: String,
-    text2: String,
     keyboard: Keyboard,
 }
 
@@ -11,7 +10,6 @@ impl Default for MyApp {
     fn default() -> Self {
         Self {
             text: "edit me".to_string(),
-            text2: "edit me".to_string(),
             keyboard: Keyboard::default(),
         }
     }
@@ -20,17 +18,18 @@ impl Default for MyApp {
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         CentralPanel::default().show(ctx, |ui| {
+            // Inject text events into Egui context. This function needs to be called before any widget is
+            // created, otherwise the key presses will be ignored.
             self.keyboard.pump_events(ctx);
-            ui.heading("egui keyboard demo");
-            ui.text_edit_singleline(&mut self.text);
 
             Window::new("Hello")
                 .constrain_to(self.keyboard.safe_rect(ctx))
                 .show(ui.ctx(), |ui| {
                     ui.label("This window is constrained to the \"safe\" area, which is not covered by the keyboard.");
-                    ui.text_edit_singleline(&mut self.text2);
+                    ui.text_edit_singleline(&mut self.text);
                 });
 
+            // Show the keyboard.
             self.keyboard.show(ctx);
         });
     }
